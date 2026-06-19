dd9e5a96a43e9014f6208c5dcb647edb8f25306c
leancodig2-pixel <leancodig2@gmail.com>
2026-06-19 21:13:50 +0200

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'super_secret_key_corporativa_xyz_2026'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///usuarios.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
